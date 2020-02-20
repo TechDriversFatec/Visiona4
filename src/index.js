eb9320ee30fd596bf6d893855e9865584e219ad1
Willyan Antunes <willyan.dantunes@gmail.com>
2020-02-20 11:14:26 -0300

    :rocket: add Hashed router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
 import Router from './router'
 
-ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><Router /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter basename={process.env.PUBLIC_URL}><Router /></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
