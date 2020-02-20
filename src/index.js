1eb46f1ee088e8ee05615ad169bd93a1b2a92d31
Willyan Antunes <willyan.dantunes@gmail.com>
2020-02-20 06:52:49 -0300

    :rocket::bug: Fix baseurl to deploy (#6)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.scss';
 import * as serviceWorker from './serviceWorker';
 import Router from './router'
 
-ReactDOM.render(<BrowserRouter><Router /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><Router /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
