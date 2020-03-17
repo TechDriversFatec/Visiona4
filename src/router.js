4b0a663228bb6d2ec8b1f1cd3ba430666e83be92
Matheusvil <matheusvilela65@hotmail.com>
2020-03-17 18:21:33 -0300

    :bug: routes correction

--- src/router.js
+++ src/router.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import {App} from './routes/App'
 import {About} from './routes/About'
 import {Map} from './routes/Map'
 
 const Router = () => (
   <Switch>
-    <Route path="/" exact component={App} />
+    <Route path="/" exact component={Map} />
     <Route path="/about" exact component={About} />
-    <Route path="/map" exact component={Map} />
   </Switch>
 )
 
