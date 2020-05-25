3102393fff696254c5be2902943f98809b163da9
iagocardial <cardialiago@gmail.com>
2020-05-25 18:37:17 -0300

    [ :lipstick:  ] added modal when clicked on AOI (#35)

--- src/Components/AOImodal/AOImodal.js
+++ src/Components/AOImodal/AOImodal.js
@@ -1,45 +1,45 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState} from "react";
-import { Modal, Icon, Button } from "semantic-ui-react";
+import "./aoiModal.scss"
+import { Modal, Icon, Button, Image} from "semantic-ui-react";
 
-const AOImodal = () => {
-  const [open, setOpen] = useState(false);
-  const closeModal = () => {
-    setOpen(false);
-  };
-  const openModal = () => setOpen(true);
+const AOImodal = (props) => {
+  const setVisible = props.visible
+  const [open, setOpen] = useState(setVisible);
+  const [close, setClose] = useState(setVisible(false));
+  console.log('asdasd', setVisible)
 
   return (
-    <div>
-      <Button icon color="grey" onClick={openModal}>
-        <Icon name="send" />
-        &nbsp;Enviar configurações
-      </Button>
-      <Modal open={open} dimmer="blurring">
+    <div className="containerAOI">
+      <Modal open={setOpen} dimmer="blurring"> 
         <Modal.Header>
           <p style={{ width: "100%" }}>
-            Selecione um Modal
+            Selecione uma Imagem
             <Icon
               name="times"
               link
               style={{ position: "absolute", right: "10px" }}
-              onClick={closeModal}
+              onClick={()=> setClose(false)}
             />
           </p>
         </Modal.Header>
         <Modal.Content>
           <Modal.Description>
-
+          <Image
+              wrapped
+              size='small'
+              src='https://react.semantic-ui.com/images/avatar/large/rachel.png'
+            />
           </Modal.Description>
         </Modal.Content>
         <Modal.Actions>
-          <Button color="red" onClick={closeModal}>
+          <Button color="red" onClick={()=> setClose(false)}>
             <Icon name="remove" />
             Cancelar
           </Button>
-          <Button color="green" onClick={closeModal}>
+          <Button color="green" onClick={()=> setClose(false)}>
             <Icon name="checkmark" />
-            Selecionar
+            Confirmar
           </Button>
         </Modal.Actions>
       </Modal>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import GeojsonUploadModal from "../GeojsonUploadModal";
 import CloudCoverage from '../CloudCoverage/CloudCoverage'
-import AOImodal from '../AOImodal/AOImodal'
+// import AOImodal from '../AOImodal/AOImodal'
 import './Footer.scss'
 
 const Footer = () => (
   <div className="footer">
     <GeojsonUploadModal />
     <CloudCoverage />
-    <AOImodal />
   </div>
 );
 
